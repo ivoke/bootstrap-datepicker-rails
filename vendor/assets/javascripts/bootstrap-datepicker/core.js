ab11b916ede5efced09affb6f9e7964bb7031bb1
Fabian Fetik <fabian.fetik@ivoke.de>
2012-12-17 17:58:01 +0100

    fix

--- vendor/assets/javascripts/bootstrap-datepicker/core.js
+++ vendor/assets/javascripts/bootstrap-datepicker/core.js
@@ -238,11 +238,11 @@
         }
       }
       else {
-        var selection = new UTCDate(this.date),
+        var selection = UTCDate(this.date),
             firstDay = selection.getDate() - selection.getDay() + 1,
             lastDay = selection.getDate() - selection.getDay() + 7,
-            firstDayDate = DPGlobal.formatDate(new UTCDate(selection.getFullYear(), selection.getMonth() + 1, firstDay), this.format, this.language),
-            lastDayDate = DPGlobal.formatDate(new UTCDate(selection.getFullYear(), selection.getMonth() + 1, lastDay), this.format, this.language);
+            firstDayDate = DPGlobal.formatDate(UTCDate(selection.getFullYear(), selection.getMonth() + 1, firstDay), this.format, this.language),
+            lastDayDate = DPGlobal.formatDate(UTCDate(selection.getFullYear(), selection.getMonth() + 1, lastDay), this.format, this.language);
         console.log("selection", selection, "firstDay", firstDay, "lastDay", lastDay, "firstDayDate", firstDayDate, "lastDayDate", lastDayDate);
         if (!this.isInput) {
           if (this.component){
